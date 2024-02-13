c58d42fdfe7f3822b62a405974abf4d48437cf07
jin <jin>
2024-02-13 15:27:35 +0000

    modified blackbox.sh kernel hello

--- ci/blackbox.sh
+++ ci/blackbox.sh
@@ -154,6 +154,9 @@ then
 elif [ -d "$VORTEX_HOME/tests/regression/$APP" ];
 then
     APP_PATH=$VORTEX_HOME/tests/regression/$APP
+elif [ -d "$VORTEX_HOME/tests/kernel/$APP" ];
+then
+    APP_PATH=$VORTEX_HOME/tests/kernel/$APP
 else
     echo "Application folder not found: $APP"
     exit -1
